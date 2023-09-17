6b38734187ca50763fe8f35567371f6a3e6faffc
jane05070 <jane05070@gmail.com>
2023-09-17 22:34:05 +0300

    add container

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -2,17 +2,17 @@ import styled from '@emotion/styled';
 
 export const MainContainer = styled.div`
   min-width: 320px;
-   padding: 0 10px;
+  //  padding: 0 10px;
    margin: 0 auto;
 
   @media (min-width: 768px) {
     min-width: 768px;
-    padding: 0 20px;
+    // padding: 0 20px;
     margin: 0 auto;
   }
   @media (min-width: 1440px) {
     max-width: 1440px;
-    padding: 0 20px;
+    // padding: 0 20px;
     margin: 0 auto;
   }
 `;
